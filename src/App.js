eecb0b02f45e8b1e0a5a5e1194920d11fee26500
B-Eulalia <101184278+B-Eulalia@users.noreply.github.com>
2022-06-11 16:48:05 +0300

    dictionary_react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import AddList from './component/addElementList';
+import Title from './component/title';
+import SearchW from './component/searchWord';
+
+function App () {
+return (
+<p>
+  <Title />
+  <AddList />
+  <SearchW />
+  </p>
+  );
+}
+export default App;
---
+++ src/component/title.js
@@ -0,0 +1,6 @@
+ import React from "react";
+ 
+ function Title() {
+     return <h1>Dictionary</h1>
+ }
+ export default Title;
